bbb37cf6ea6bc72bd9439fe8add1c9841fbe1948
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-06-24 17:10:55 -0300

    feat: rota de usuários e suas validações

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString({ message: 'A senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'A senha é obrigatória' })
+  @MinLength(8, { message: 'A senha precisa ter no mínimo 8 caracteres' })
+  password: string;
+}
