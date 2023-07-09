cd5bf0a03242c04a0d9da0ffc28cae6468eba832
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-07-09 00:57:45 -0300

    feat: metade and router me

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/shared/database/repositories/users.repositories';
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersRepo: UserRepository) {}
+
   getUserById(userId: string) {
-    return userId;
+    return this.usersRepo.findUnique({
+      where: { id: userId },
+      select: {
+        name: true,
+        email: true,
+      },
+    });
   }
 }
